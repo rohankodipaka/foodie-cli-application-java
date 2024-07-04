f707a6777a709bc3f1d8d9daf189ffcac5c3b453
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-07-04 19:33:14 +0530

    feat: Add DishRepository.java file

---
+++ src/com/javaproject/foodiecliapplication/repository/DishRepository.java
@@ -0,0 +1,46 @@
+package com.javaproject.foodiecliapplication.repository;
+
+import com.javaproject.foodiecliapplication.model.Dish;
+import com.javaproject.foodiecliapplication.util.CsvReader;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DishRepository {
+
+    private List<Dish> dishesList;
+
+    public DishRepository() {
+        CsvReader csvReader = new CsvReader();
+        this.dishesList = csvReader.readDishesFromCsv();
+    }
+
+    public Dish save(Dish dish){
+        this.dishesList.add(dish);
+        return dish;
+    }
+
+    public Optional<Dish> getDishById(String id){
+        return this.dishesList.stream().filter(dish -> dish.getId().equals(id)).findFirst();
+    }
+
+    public Optional<Dish> updateDish(Dish dishToBeUpdated){
+        return this.dishesList.stream() .filter(dish -> dish.getId() == dishToBeUpdated.getId())
+                .findFirst()
+                .map(dish -> {
+                    dish.setName(dishToBeUpdated.getName());
+                    dish.setId(dishToBeUpdated.getId());
+                    dish.setDescription(dishToBeUpdated.getDescription());
+                    dish.setPrice(dishToBeUpdated.getPrice());
+                    return dish;
+        });
+    }
+
+    public void deleteDish(Dish dish){
+        dishesList.remove(dish);
+    }
+
+
+
+
+}
