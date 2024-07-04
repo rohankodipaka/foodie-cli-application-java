ef3db980713e5c776d9c81a904b6acd0242567af
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-07-04 17:20:20 +0530

    feat: Add Main.java to displayMainMenu

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import com.javaproject.foodiecliapplication.repository.CustomerRepository;
+import com.javaproject.foodiecliapplication.ui.Menu;
+import com.javaproject.foodiecliapplication.util.CsvReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /* CsvReader csvReader = new CsvReader();
+        System.out.println(csvReader.readCustomerFromCsv()); */
+
+        // CustomerRepository customerRepository = new CustomerRepository();
+        // System.out.println(customerRepository.getAllCustomers());
+
+        Menu menu = new Menu();
+        menu.displayMainMenu();
+
+    }
+}
