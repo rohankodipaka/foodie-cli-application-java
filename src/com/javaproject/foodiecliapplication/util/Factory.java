11f6a0b5d05b7f51ba8cd3ec0fb7a27af34a359a
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-07-05 23:58:38 +0530

    feat: Update Factory.java file

--- src/com/javaproject/foodiecliapplication/util/Factory.java
+++ src/com/javaproject/foodiecliapplication/util/Factory.java
@@ -1,8 +1,11 @@
 package com.javaproject.foodiecliapplication.util;
 
 import com.javaproject.foodiecliapplication.controller.CustomerController;
+import com.javaproject.foodiecliapplication.controller.DishController;
 import com.javaproject.foodiecliapplication.repository.CustomerRepository;
+import com.javaproject.foodiecliapplication.repository.DishRepository;
 import com.javaproject.foodiecliapplication.service.CustomerServiceImpl;
+import com.javaproject.foodiecliapplication.service.DishServiceImpl;
 
 public class Factory {
 
@@ -11,12 +14,23 @@ public class Factory {
     }
 
     public static CustomerServiceImpl getCustomerService(){
-
         return new CustomerServiceImpl(getCustomerRepository());
     }
 
     public static CustomerController getCustomerController(){
         return new CustomerController(getCustomerService());
+    }
+
+    public static DishRepository getDishRepository(){
+        return new DishRepository();
+    }
+
+    public static DishServiceImpl getDishService(){
+        return new DishServiceImpl(getDishRepository());
+    }
+
+    public static DishController getDishController(){
+        return new DishController(getDishService());
     }
 
 }
