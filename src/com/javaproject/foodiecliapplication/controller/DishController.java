196ac3ac31380b5d4eb3db05a6638c2233aa3cfd
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-07-05 23:57:31 +0530

    feat: Update DishController.java file

--- src/com/javaproject/foodiecliapplication/controller/DishController.java
+++ src/com/javaproject/foodiecliapplication/controller/DishController.java
@@ -1,8 +1,11 @@
 package com.javaproject.foodiecliapplication.controller;
 
 import com.javaproject.foodiecliapplication.exceptions.DishAlreadyExistsException;
+import com.javaproject.foodiecliapplication.exceptions.DishNotFoundException;
 import com.javaproject.foodiecliapplication.model.Dish;
 import com.javaproject.foodiecliapplication.service.DishServiceImpl;
+
+import java.util.List;
 
 public class DishController {
 
@@ -12,8 +15,26 @@ public class DishController {
         this.dishService = dishService;
     }
 
+
+    public List<Dish> getDishesList(){
+        return this.dishService.getDishesList();
+    }
+
     public Dish save(Dish dish) throws DishAlreadyExistsException{
         return this.dishService.save(dish);
     }
+
+    public Dish getDishId(String Id) throws DishNotFoundException {
+        return this.dishService.getDishById(Id);
+    }
+
+    public Dish updateDish(Dish dishToBeUpdated) throws DishNotFoundException{
+        return this.dishService.updateDish(dishToBeUpdated);
+    }
+
+    public Dish deleteDish(Dish dish){
+        return this.dishService.deleteDish(dish);
+    }
+
 
 }
