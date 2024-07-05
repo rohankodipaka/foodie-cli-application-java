99ad90b3c7aa04043b4c4b2bf2f3fe7b66afc4df
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-07-05 23:58:05 +0530

    feat: Add DishNotFoundException.java file

---
+++ src/com/javaproject/foodiecliapplication/exceptions/DishNotFoundException.java
@@ -0,0 +1,8 @@
+package com.javaproject.foodiecliapplication.exceptions;
+
+public class DishNotFoundException extends Exception{
+
+    public DishNotFoundException(String message) {
+        super(message);
+    }
+}
