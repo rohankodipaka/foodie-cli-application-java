36d931955b64ccb802ffd3995f4f5e5f52bcce48
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-07-06 14:42:51 +0530

    feat: Add customer and Dish components

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,14 @@
 import com.javaproject.foodiecliapplication.repository.CustomerRepository;
+import com.javaproject.foodiecliapplication.repository.DishRepository;
 import com.javaproject.foodiecliapplication.ui.Menu;
 import com.javaproject.foodiecliapplication.util.CsvReader;
 
 public class Main {
     public static void main(String[] args) {
 
-        /* CsvReader csvReader = new CsvReader();
-        System.out.println(csvReader.readCustomerFromCsv()); */
-
-        // CustomerRepository customerRepository = new CustomerRepository();
-        // System.out.println(customerRepository.getAllCustomers());
-
-        // 1. check customer registration and login.
         Menu menu = new Menu();
         menu.displayMainMenu();
+
 
     }
 }
