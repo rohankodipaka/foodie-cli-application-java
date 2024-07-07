20bc514803c5e85504dd2ba124a2bf53a68e8fed
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-07-07 22:57:57 +0530

    feat: Add RestaurantAlreadyExistException.java and RestaurantNotFoundException.java

---
+++ src/com/javaproject/foodiecliapplication/exceptions/RestaurantAlreadyExistException.java
@@ -0,0 +1,8 @@
+package com.javaproject.foodiecliapplication.exceptions;
+
+public class RestaurantAlreadyExistException extends Exception{
+
+    public RestaurantAlreadyExistException(String message) {
+        super(message);
+    }
+}
---
+++ src/com/javaproject/foodiecliapplication/exceptions/RestaurantNotFoundException.java
@@ -0,0 +1,8 @@
+package com.javaproject.foodiecliapplication.exceptions;
+
+public class RestaurantNotFoundException extends Exception{
+
+    public RestaurantNotFoundException(String message) {
+        super(message);
+    }
+}
