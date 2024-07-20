1fca709b9833f30838275453a983d92ad3705355
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-07-20 23:08:50 +0530

    feat: update Factory.java

--- src/com/javaproject/foodiecliapplication/util/Factory.java
+++ src/com/javaproject/foodiecliapplication/util/Factory.java
@@ -13,54 +13,83 @@ import com.javaproject.foodiecliapplication.service.DishServiceImpl;
 import com.javaproject.foodiecliapplication.service.OrderServiceImpl;
 import com.javaproject.foodiecliapplication.service.RestaurantServiceImpl;
 
+
 public class Factory {
 
-    public static CustomerRepository getCustomerRepository(){
-        return new CustomerRepository();
+    public static CsvReader getCsvReader() {
+        return Holder.CSV_READER;
     }
 
-    public static CustomerServiceImpl getCustomerService(){
-        return new CustomerServiceImpl(getCustomerRepository());
+    public static CustomerRepository getCustomerRepository() {
+        return Holder.CUSTOMER_REPOSITORY;
     }
 
-    public static CustomerController getCustomerController(){
-        return new CustomerController(getCustomerService());
+    public static CustomerServiceImpl getCustomerService() {
+        return Holder.CUSTOMER_SERVICE;
     }
 
-    public static DishRepository getDishRepository(){
-        return new DishRepository();
+    public static CustomerController getCustomerController() {
+        return Holder.CUSTOMER_CONTROLLER;
     }
 
-    public static DishServiceImpl getDishService(){
-        return new DishServiceImpl(getDishRepository());
+    public static DishRepository getDishRepository() {
+        return Holder.DISH_REPOSITORY;
     }
 
-    public static DishController getDishController(){
-        return new DishController(getDishService());
+    public static DishServiceImpl getDishService() {
+        return Holder.DISH_SERVICE;
     }
 
-    public static CsvReader getCsvReader(){
-        return new CsvReader();
+    public static DishController getDishController() {
+        return Holder.DISH_CONTROLLER;
     }
 
-    public static RestaurantRepository getRestaurantRepository(){
-        return new RestaurantRepository();
-    }
-    public static RestaurantServiceImpl getRestaurantService(){
-        return new RestaurantServiceImpl(getRestaurantRepository());
-    }
-    public static RestaurantController getRestaurantController(){
-        return new RestaurantController(getRestaurantService());
+    public static RestaurantRepository getRestaurantRepository() {
+        return Holder.RESTAURANT_REPOSITORY;
     }
 
-    public static OrderRepository getOrderRepository(){
-        return new OrderRepository();
+    public static RestaurantServiceImpl getRestaurantService() {
+        return Holder.RESTAURANT_SERVICE;
     }
-    public static OrderServiceImpl getOrderService(){
-        return new OrderServiceImpl(getOrderRepository());
+
+    public static RestaurantController getRestaurantController() {
+        return Holder.RESTAURANT_CONTROLLER;
     }
-    public static OrderController getOrderController(){
-        return new OrderController(getOrderService());
+
+    public static OrderRepository getOrderRepository() {
+        return Holder.ORDER_REPOSITORY;
     }
+
+    public static OrderServiceImpl getOrderService() {
+        return Holder.ORDER_SERVICE;
+    }
+
+    public static OrderController getOrderController() {
+        return Holder.ORDER_CONTROLLER;
+    }
+
+    private static class Holder {
+
+        private static final CsvReader CSV_READER = new CsvReader();
+
+        private static final CustomerRepository CUSTOMER_REPOSITORY = new CustomerRepository();
+        private static final CustomerServiceImpl CUSTOMER_SERVICE = new CustomerServiceImpl(CUSTOMER_REPOSITORY);
+        private static final CustomerController CUSTOMER_CONTROLLER = new CustomerController(CUSTOMER_SERVICE);
+
+        private static final DishRepository DISH_REPOSITORY = new DishRepository();
+        private static final DishServiceImpl DISH_SERVICE = new DishServiceImpl(DISH_REPOSITORY);
+        private static final DishController DISH_CONTROLLER = new DishController(DISH_SERVICE);
+
+        private static final RestaurantRepository RESTAURANT_REPOSITORY = new RestaurantRepository();
+        private static final RestaurantServiceImpl RESTAURANT_SERVICE = new RestaurantServiceImpl(RESTAURANT_REPOSITORY);
+        private static final RestaurantController RESTAURANT_CONTROLLER = new RestaurantController(RESTAURANT_SERVICE);
+
+        private static final OrderRepository ORDER_REPOSITORY = new OrderRepository();
+        private static final OrderServiceImpl ORDER_SERVICE = new OrderServiceImpl(ORDER_REPOSITORY);
+        private static final OrderController ORDER_CONTROLLER = new OrderController(ORDER_SERVICE);
+
+    }
+
 
 }
+
